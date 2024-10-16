a49fb020679b0b92d76a958ad9dc3191b9517ebc
Corentin Néau <tan.neau@suse.com>
2024-10-16 18:42:55 +0200

    Move fleetyaml unit tests to package `fleetyaml_test`
    This may prevent unnecessary imports into Fleet binaries.

--- internal/fleetyaml/fleetyaml_test.go
+++ internal/fleetyaml/fleetyaml_test.go
@@ -1,13 +1,15 @@
 //go:build !windows
 // +build !windows
 
-package fleetyaml
+package fleetyaml_test
 
 import (
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/rancher/fleet/internal/fleetyaml"
 )
 
 func TestBundleYaml(t *testing.T) {
@@ -15,15 +17,15 @@ func TestBundleYaml(t *testing.T) {
 	for _, path := range []string{"/foo", "foo", "/foo/", "foo/", "../foo/bar"} {
 
 		// Test both the primary extension and the fallback extension.
-		for _, fullPath := range []string{GetFleetYamlPath(path, false), GetFleetYamlPath(path, true)} {
-			a.True(IsFleetYaml(filepath.Base(fullPath)))
-			a.True(IsFleetYamlSuffix(fullPath))
+		for _, fullPath := range []string{fleetyaml.GetFleetYamlPath(path, false), fleetyaml.GetFleetYamlPath(path, true)} {
+			a.True(fleetyaml.IsFleetYaml(filepath.Base(fullPath)))
+			a.True(fleetyaml.IsFleetYamlSuffix(fullPath))
 		}
 	}
 
 	// Test expected failure payloads.
 	for _, fullPath := range []string{"fleet.yaaaaaaaaaml", "", ".", "weakmonkey.yaml", "../fleet.yaaaaml"} {
-		a.False(IsFleetYaml(filepath.Base(fullPath)))
-		a.False(IsFleetYamlSuffix(fullPath))
+		a.False(fleetyaml.IsFleetYaml(filepath.Base(fullPath)))
+		a.False(fleetyaml.IsFleetYamlSuffix(fullPath))
 	}
 }
