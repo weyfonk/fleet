f3bd663ca63f8bdf61757ea9456c946ac723c515
Corentin Néau <tan.neau@suse.com>
2024-10-16 18:03:05 +0200

    Move objectset unit tests to package `objectset_test`
    This may prevent unnecessary imports into Fleet binaries.

--- internal/cmd/agent/deployer/objectset/objectset_test.go
+++ internal/cmd/agent/deployer/objectset/objectset_test.go
@@ -1,52 +1,54 @@
-package objectset
+package objectset_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/rancher/fleet/internal/cmd/agent/deployer/objectset"
 )
 
 func TestObjectByKey_Namespaces(t *testing.T) {
 	tests := []struct {
 		name           string
-		objects        ObjectByKey
+		objects        objectset.ObjectByKey
 		wantNamespaces []string
 	}{
 		{
 			name:           "empty",
-			objects:        ObjectByKey{},
+			objects:        objectset.ObjectByKey{},
 			wantNamespaces: nil,
 		},
 		{
 			name: "1 namespace",
-			objects: ObjectByKey{
-				ObjectKey{Namespace: "ns1", Name: "a"}: nil,
-				ObjectKey{Namespace: "ns1", Name: "b"}: nil,
+			objects: objectset.ObjectByKey{
+				objectset.ObjectKey{Namespace: "ns1", Name: "a"}: nil,
+				objectset.ObjectKey{Namespace: "ns1", Name: "b"}: nil,
 			},
 			wantNamespaces: []string{"ns1"},
 		},
 		{
 			name: "many namespaces",
-			objects: ObjectByKey{
-				ObjectKey{Namespace: "ns1", Name: "a"}: nil,
-				ObjectKey{Namespace: "ns2", Name: "b"}: nil,
+			objects: objectset.ObjectByKey{
+				objectset.ObjectKey{Namespace: "ns1", Name: "a"}: nil,
+				objectset.ObjectKey{Namespace: "ns2", Name: "b"}: nil,
 			},
 			wantNamespaces: []string{"ns1", "ns2"},
 		},
 		{
 			name: "many namespaces with duplicates",
-			objects: ObjectByKey{
-				ObjectKey{Namespace: "ns1", Name: "a"}: nil,
-				ObjectKey{Namespace: "ns2", Name: "b"}: nil,
-				ObjectKey{Namespace: "ns1", Name: "c"}: nil,
+			objects: objectset.ObjectByKey{
+				objectset.ObjectKey{Namespace: "ns1", Name: "a"}: nil,
+				objectset.ObjectKey{Namespace: "ns2", Name: "b"}: nil,
+				objectset.ObjectKey{Namespace: "ns1", Name: "c"}: nil,
 			},
 			wantNamespaces: []string{"ns1", "ns2"},
 		},
 		{
 			name: "missing namespace",
-			objects: ObjectByKey{
-				ObjectKey{Namespace: "ns1", Name: "a"}: nil,
-				ObjectKey{Name: "b"}:                   nil,
+			objects: objectset.ObjectByKey{
+				objectset.ObjectKey{Namespace: "ns1", Name: "a"}: nil,
+				objectset.ObjectKey{Name: "b"}:                   nil,
 			},
 			wantNamespaces: []string{"", "ns1"},
 		},
